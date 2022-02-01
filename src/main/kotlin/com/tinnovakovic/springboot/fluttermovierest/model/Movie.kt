2e681e7ee8b33d70269f7ac6753ae72445c8519e
Tin Novakovic <tin.novakovic@bglgroup.co.uk>
2022-02-01 17:05:36 +0000

    ONE TO ONE Relationship for Movie --> MovieDetail Completed

--- src/main/kotlin/com/tinnovakovic/springboot/fluttermovierest/model/Movie.kt
+++ src/main/kotlin/com/tinnovakovic/springboot/fluttermovierest/model/Movie.kt
@@ -3,14 +3,17 @@ package com.tinnovakovic.springboot.fluttermovierest.model
 import javax.persistence.*
 
 @Entity
-@Table(name = "movie", uniqueConstraints = [UniqueConstraint(name = "unique_movie_constraints", columnNames = ["movieId"])])
+@Table(name = "movie", uniqueConstraints = [UniqueConstraint(name = "unique_movie_constraints", columnNames = ["movie_id"])])
 data class Movie(
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", updatable = false)
     val id: Int = -1,
-    @Column(name = "movieId", nullable = false, columnDefinition = "TEXT")
+    @Column(name = "movie_id", nullable = false, columnDefinition = "TEXT")
     val movieId: String = "",
-    @Column(name = "posterPath", nullable = false, columnDefinition = "TEXT")
-    val posterPath: String = ""
+    @Column(name = "poster_path", nullable = false, columnDefinition = "TEXT")
+    val posterPath: String = "",
+    @OneToOne(cascade = [CascadeType.ALL])
+    @JoinColumn(name = "movie_detail_id", referencedColumnName = "id")
+    var movieDetail: MovieDetail = MovieDetail(movieId = movieId, posterPath = posterPath)
 )
--- src/main/kotlin/com/tinnovakovic/springboot/fluttermovierest/model/MovieDetail.kt
+++ src/main/kotlin/com/tinnovakovic/springboot/fluttermovierest/model/MovieDetail.kt
@@ -5,28 +5,28 @@ import javax.persistence.*
 @Entity
 @Table(
     name = "movie_detail",
-    uniqueConstraints = [UniqueConstraint(name = "unique_movieDetail_constraints", columnNames = ["movieId"])]
+    uniqueConstraints = [UniqueConstraint(name = "unique_movieDetail_constraints", columnNames = ["movie_id"])]
 )
 data class MovieDetail(
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     val id: Int = -1,
-    @Column(name = "movieId")
+    @Column(name = "movie_id")
     val movieId: String = "",
     @Column(name = "title")
     val title: String = "",
     @Column(name = "overview")
     val overview: String = "",
-    @Column(name = "posterPath")
+    @Column(name = "poster_path")
     val posterPath: String = "",
-    @Column(name = "backdropPath")
+    @Column(name = "backdrop_path")
     val backdropPath: String = "",
     @Column(name = "directors")
     val directors: String = "",
     @Column(name = "popularity")
     val popularity: Double = -1.0,
-    @Column(name = "releaseDate")
+    @Column(name = "release_date")
     val releaseDate: String = "",
     @Column(name = "revenue")
     val revenue: Double = -1.0,
@@ -34,11 +34,11 @@ data class MovieDetail(
     val runtime: String = "",
     @Column(name = "tagline")
     val tagline: String = "",
-    @Column(name = "voteAverage")
+    @Column(name = "vote_average")
     val voteAverage: Double = -1.0,
-    @Column(name = "voteCount")
+    @Column(name = "vote_count")
     val voteCount: Int = -1,
-    @Column(name = "isFavourite")
+    @Column(name = "is_favourite")
     val isFavourite: Boolean = false
 )
 
