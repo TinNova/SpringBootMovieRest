9795f87ea9b67d110684605ea9d7c10995689447
Tin Novakovic <tin.novakovic@comparethemarket.com>
2023-02-15 22:04:53 +0000

    Allowing anyone to create a new user and preventing a token being required to do it.

--- src/main/kotlin/com/tinnovakovic/springboot/fluttermovierest/security/SecurityConfig.kt
+++ src/main/kotlin/com/tinnovakovic/springboot/fluttermovierest/security/SecurityConfig.kt
@@ -41,7 +41,7 @@ class SecurityConfig(
             http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
             http.authorizeRequests().antMatchers(HttpMethod.GET, "/api/login/**", "/api/users/token/refresh/**").permitAll()
             http.authorizeRequests().antMatchers(HttpMethod.GET, "/api/user/**").hasAnyAuthority(ROLE_USER)
-            http.authorizeRequests().antMatchers(HttpMethod.POST, "/api/user/save/**").hasAnyAuthority(ROLE_ADMIN)
+            http.authorizeRequests().antMatchers(HttpMethod.POST, "/api/users/save/**").permitAll()
             http.authorizeRequests().anyRequest().authenticated()
             http.addFilter(customAuthenticationFilter)
             http.addFilterBefore(CustomerAuthorisationFilter(), UsernamePasswordAuthenticationFilter().javaClass)
